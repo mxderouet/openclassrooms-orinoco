d9418d8cf5af13399fce14916623bf54560e7796
maxime <mxderouet@gmail.com>
2020-11-05 15:41:28 +0100

    api connect

--- cameras.js
+++ cameras.js
@@ -1,13 +1,44 @@
-const url = 'http://localhost:3000/api/cameras';
+const url = 'http://localhost:3000/api/cameras/';
 
-fetch(url)
-    .then(function(data) {
-        console.log(data);
-    })
-    .catch(function(error) {
-        if(!data) {
-            console.log('error : no data recieved from API');
-        }
-});
+getAllCameras = () => {
+  return new Promise((resolve) => {
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+      if (
+        this.readyState == XMLHttpRequest.DONE &&
+        this.status >= 200 &&
+        this.status < 400
+      ) {
+        resolve(JSON.parse(this.responseText));
+        console.log("Connecté");
+      } else {
+        console.log("Non connecté");
+      }
+    };
+    try {
+      request.open("GET", url);
+      request.send();
+    } catch (e) {
+      console.error(e);
+    } finally {
+      console.log("ICI2");
+    }
+    
+  });
+};
+
+getAllCameras();
+// xmlhttp.send();
+// request.send()
+
+// fetch(url)
+//     .then(function(data) {
+//         console.log(data);
+//     })
+//     .catch(function(error) {
+//         if(!data) {
+//             console.log('error : no data recieved from API');
+//         }
+// });
 
 console.log('ICI');
